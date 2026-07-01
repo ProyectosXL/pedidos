023a4ab9a784e2a2185625dde7b3af31cf271109
Gaston Marcilio <gaston.marcilio@gmail.com>
2026-07-01 09:24:21 -0300

    fix pedidos

--- grupo/pedidos/accesorios.php
+++ grupo/pedidos/accesorios.php
@@ -419,8 +419,8 @@ include_once __DIR__.'/../../class/pedido.php';
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="js/main.js"></script>
-    <script src="../../pedidos/js/envio.js"></script>
-    <script src="../../pedidos/js/jquery.table2excel.js"></script>
+    <!-- <script src="../../pedidos/js/envio.js"></script>
+    <script src="../../pedidos/js/jquery.table2excel.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
         $(document).ready(function() {
--- grupo/pedidos/general.php
+++ grupo/pedidos/general.php
@@ -264,8 +264,8 @@ include_once __DIR__.'/../../class/pedido.php';
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
     <script src="js/main.js?v=<?= filemtime(__DIR__ . '/js/main.js') ?>"></script>
-    <script src="../../pedidos/js/envio.js"></script>
-    <script src="../../pedidos/js/jquery.table2excel.js"></script>
+    <!-- <script src="../../pedidos/js/envio.js"></script>
+    <script src="../../pedidos/js/jquery.table2excel.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
         $(document).ready(function() {
